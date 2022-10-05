b0c02109f7e3abed2104ff038d0afab6f0f24406
manosrija-nasrin <manosrijanasrin@gmail.com>
2022-10-05 19:53:02 +0530

    set-rx
    call editorRowCxToRx() at the top of editorScroll() to finally set E.rx to its proper value

--- kilo.c
+++ kilo.c
@@ -202,7 +202,7 @@ void editorUpdateRow(erow *row) {
 }
 
 void editorAppendRow(char *s, size_t len) {
- E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
   int at = E.numrows;
   E.row[at].size = len;
@@ -257,7 +257,10 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 void editorScroll() {
-  E.rx = E.cx;
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
