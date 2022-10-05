4852842ce9feda47852145eec02dc46198d90213
manosrija-nasrin <manosrijanasrin@gmail.com>
2022-10-05 11:13:52 +0530

    welcome
    add welcome message a third of the way down the screen

--- kilo.c
+++ kilo.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 /*** defines ***/
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -114,7 +115,15 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
 		abAppend(ab, "\x1b[K", 3);  //erase in line
     if (y < E.screenrows - 1) {
