ba86d4b396f1278de2e01f9a4629dd17308fba4e
manosrija-nasrin <manosrijanasrin@gmail.com>
2022-10-05 20:06:39 +0530

    status-bar-make-room

--- kilo.c
+++ kilo.c
@@ -303,9 +303,7 @@ void editorDrawRows(struct abuf *ab) {
     }
 
 		abAppend(ab, "\x1b[K", 3);  //erase in line
-    if (y < E.screenrows - 1) {
-      abAppend(ab, "\r\n", 2);
-    }
+    abAppend(ab, "\r\n", 2);
   }
 }
 
@@ -422,6 +420,7 @@ void initEditor() {
 	E.row = NULL;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+	E.screenrows -= 1;
 }
 
 int main(int argc, char *argv[]) {
