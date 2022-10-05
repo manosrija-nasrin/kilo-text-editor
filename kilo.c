10c5b40ffb334ebf1b0c5b852c25e09213d4fe84
manosrija-nasrin <manosrijanasrin@gmail.com>
2022-10-05 10:04:49 +0530

    error-handling

--- kilo.c
+++ kilo.c
@@ -1,9 +1,9 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-
 
 struct termios orig_termios;
 
@@ -13,38 +13,37 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
 
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);    // input flags
-	raw.c_oflag &= ~(OPOST);     // output flags 
-  raw.c_cflag |= (CS8);        // control flags
-	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);    // local/misc. flags
-  // control characters
-  raw.c_cc[VMIN] = 0;   // VMIN: sets the min no. of bytes of 
-                        // input needed before read() can return
-  raw.c_cc[VTIME] = 1;   // VTIME: sets the max amt of time to 
-                        // wait before read() returns
-  
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main() {
   enableRawMode();
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   }
   return 0;
 }
