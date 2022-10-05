c60ba92bb781708512cb46083e27f92cae3245ca
manosrija-nasrin <manosrijanasrin@gmail.com>
2022-10-05 11:11:23 +0530

    clear-line
    erase line by line instead of erasing the entire screen all at once

--- kilo.c
+++ kilo.c
@@ -115,6 +115,8 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
+
+		abAppend(ab, "\x1b[K", 3);  //erase in line
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -125,7 +127,6 @@ void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
 
 	abAppend(&ab, "\x1b[?25l", 6);
-  abAppend(&ab, "\x1b[2J", 4);  //clear screen
   abAppend(&ab, "\x1b[H", 3);   //move cursor to top
 
   editorDrawRows(&ab);          //draw tildes for entire screen
