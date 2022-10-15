848ab35062336fac5a624137a55d7da02bfe855e
Manosrija Nasrin <manosrijanasrin@gmail.com>
2022-10-16 00:24:54 +0530

    save-errors
    add error handling to editorSave()

--- kilo.c
+++ kilo.c
@@ -282,10 +282,17 @@ void editorSave() {
   if (E.filename == NULL) return;
   int len;
   char *buf = editorRowsToString(&len);
-  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);   //0644 - standard permission for file access
-  ftruncate(fd, len);
-  write(fd, buf, len);
-  close(fd);
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  if (fd != -1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        return;
+      }
+    }
+    close(fd);
+  }
   free(buf);
 }
 
