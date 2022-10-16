c49d4cabafff3d7b252e5f573cb5f418c090dc7b
manosrija-nasrin <manosrijanasrin@gmail.com>
2022-10-16 12:37:30 +0530

    syntax-refactoring
    hl: an array of unsigned char to store the highlighting of each line in an array

--- kilo.c
+++ kilo.c
@@ -42,6 +42,7 @@ typedef struct erow {
 	int rsize;    //render size
   char *chars;
   char *render;
+  unsigned char *hl;
 } erow;
 
 struct editorConfig {
@@ -238,6 +239,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 
 	E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
 	editorUpdateRow(&E.row[at]);
 
   E.numrows++;
@@ -247,6 +249,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 void editorFreeRow(erow *row) {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 
 void editorDelRow(int at) {
