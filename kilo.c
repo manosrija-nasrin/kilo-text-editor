929c712b061debb83a1b26ad12bd97a35816b6f3
manosrija-nasrin <manosrijanasrin@gmail.com>
2022-10-16 12:47:14 +0530

    call-update-syntax
    call editorUpdateSyntax() in editorUpdateRow()

--- kilo.c
+++ kilo.c
@@ -188,7 +188,7 @@ int getWindowSize(int *rows, int *cols) {
 void editorUpdateSyntax(erow *row) {
   row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
-  
+
   int i;
   for (i = 0; i < row->rsize; i++) {
     if (isdigit(row->render[i])) {
@@ -243,6 +243,8 @@ void editorUpdateRow(erow *row) {
 
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len) {
