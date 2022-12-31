588054fe54ae67d0d96f4201e686ee08ca05808d
Manosrija Nasrin <manosrijanasrin@gmail.com>
2022-12-31 08:22:03 +0530

    fix-comment

--- kilo.c
+++ kilo.c
@@ -837,7 +837,7 @@ void editorRefreshScreen() {
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
-  abAppend(&ab, "\x1b[?25h", 6);   //hide cursor
+  abAppend(&ab, "\x1b[?25h", 6);   //un-hide cursor
 
 	write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
